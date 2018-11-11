c456d4296a3a786de8228fb4d9c30e97d2fc2da2
Artur <artur0695@interia.pl>
2018-11-11 18:16:39 +0100

    Messeges service

--- src/main/java/org/jax/messenger/resources/MessegeResource.java
+++ src/main/java/org/jax/messenger/resources/MessegeResource.java
@@ -1,16 +1,23 @@
 package org.jax.messenger.resources;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jax.messenger.model.Message;
+import org.jax.messenger.service.MessageService;
+
 @Path("/messeges")
 public class MessegeResource {
 
+	MessageService messageSerivce = new MessageService();
+	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getMesseges() {
-		return "Hello Worlds!";
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMesseges() {
+		return messageSerivce.getAllMessages();
 	}
 }
